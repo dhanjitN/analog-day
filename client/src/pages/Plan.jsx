b2c51e9e7c3f1ea93d60e06eedd1afb91a9baa6b
dhanjitN <dhanjitxnath@gmail.com>
2026-08-14 09:51:15 +0530

    add: tl draw license key

--- client/src/pages/Plan.jsx
+++ client/src/pages/Plan.jsx
@@ -6,7 +6,7 @@ export function PlanPage(){
     const {date} = useParams();
     return(
         <div className="fixed inset-0">
-             <Tldraw key={date} persistenceKey={`day-${date}`} />
+             <Tldraw licenseKey="tldraw-2026-08-28/WyJnVUNYcUl0ZCIsWyIqIl0sMTYsIjIwMjYtMDgtMjgiXQ.4qkcULlEIeGoSEkYCam+PRdv37LP0iGBGEaiYyNpA0lI3l8ZAIHAJbCa49MhlLscGwSG1s/gChaKZqR0/eb2Gg" key={date} persistenceKey={`day-${date}`} />
         </div>
     )
 }
